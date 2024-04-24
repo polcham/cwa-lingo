3b4666593fa89cebc1b71389f104ed686ed5f7ae
Paul Cham <paulcham91@gmail.com>
2024-04-25 01:49:47 +0800

    Duolingo Clone - Marketing Page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -2,9 +2,5 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <Button size="lg">Click me</Button>
-    </div>
-  );
+  return <div>This is marketing page</div>;
 }
