b94822ac6c2f98d5190a5e99c817e94c22097b69
Paul Cham <paulcham91@gmail.com>
2024-05-02 16:23:10 +0800

    Duolingo Clone - Main Layout

---
+++ components/mobile-header.tsx
@@ -0,0 +1,13 @@
+import MobileSidebar from "./mobile-sidebar";
+
+type Props = {
+  className?: string;
+};
+
+export const MobileHeader = ({ className }: Props) => {
+  return (
+    <nav className="lg:hidden px-6 h-[50px] flex items-center bg-green-500 border-b fixed top-0 w-full z-50">
+      <MobileSidebar />
+    </nav>
+  );
+};
